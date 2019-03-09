05d36a839fa3af1ec076493562468e09ea8cf975
shaojunda <shaojunda@gmail.com>
2019-03-09 19:11:05 +0800

    ADD: web pack on dev

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+// 定义要导出的对象
+module.exports = {
+  // 配置程序的入口 index 是文件名
+  entry: {
+    index: './lib/index.tsx'
+  },
+  // 配置输出的路径
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'SUI', // 库的名字
+    libraryTarget: 'umd' // 库的目标格式
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+}
