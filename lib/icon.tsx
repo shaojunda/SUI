e6400bbb02cf813c365395def4bccc6291b4fbc3
shaojunda <shaojunda@gmail.com>
2019-03-23 20:58:54 +0800

    ADD: onclick

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,11 +4,12 @@ import "./icon.scss";
 
 interface IconProps {
   name: string;
+  onClick: () => void;
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <svg className="sui-icon">
+    <svg className="sui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`} />
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -4,9 +4,14 @@ import Icon from "./icon";
 
 ReactDOM.render(
   <div>
-    <Icon name="wechat" />
-    <Icon name="alipay" />
-    <Icon name="qq" />
+    <Icon
+      name="wechat"
+      onClick={() => {
+        console.log("hi");
+      }}
+    />
+    {/* <Icon name="alipay" /> */}
+    {/* <Icon name="qq" /> */}
   </div>,
   document.querySelector("#root")
 );
