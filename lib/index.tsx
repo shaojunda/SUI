7fbe8692aed695effc2230efb70182f08e4bdc76
shaojunda <shaojunda@gmail.com>
2019-03-23 20:28:02 +0800

    ADD: icons

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "./button";
+import Icon from "./icon";
 
-ReactDOM.render(<Button />, document.body);
+ReactDOM.render(
+  <div>
+    <Icon name="wechat" />
+    <Icon name="alipay" />
+    <Icon name="qq" />
+  </div>,
+  document.querySelector("#root")
+);
