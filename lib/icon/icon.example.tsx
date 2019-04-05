d1ed1c18ff6b4c3a71e0efd0980982704d25b545
shaojunda <shaojunda@gmail.com>
2019-04-05 18:16:22 +0800

    feat: add examples

---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Icon from './icon';
+
+const IconExample: React.FunctionComponent = () => {
+  return <div>
+    <Icon name="alipay"></Icon>
+    <Icon name="qq"></Icon>
+    <Icon name="wechat"></Icon>
+  </div>;
+};
+
+export default IconExample;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,11 +4,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 定义要导出的对象
 module.exports = Object.assign({}, base, {
   mode: 'development',
+  entry: {
+    example: './example.tsx',
+  },
   plugins: [
     // 将 JS 插入到页面
     new HtmlWebpackPlugin({
       title: 'SUI',
-      template: 'index.html'
+      template: 'example.html'
     })
   ],
 })
