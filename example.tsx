21fe7e76f15635a8d30b3c0eab70fbea59e87bbb
shaojunda <shaojunda@gmail.com>
2019-04-27 22:54:23 +0800

    feat: add dialog example to example list

--- example.tsx
+++ example.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import {HashRouter as Router, Link, Route} from "react-router-dom";
 import IconExample from "./lib/icon/icon.example";
 import ButtonExample from "./lib/button.example";
+import DialogExample from "./lib/dialog/dialog.example";
 
 ReactDOM.render(
   <Router>
@@ -20,11 +21,15 @@ ReactDOM.render(
             <li>
               <Link to="/button">Button</Link>
             </li>
+            <li>
+              <Link to="/dialog">Dialog</Link>
+            </li>
           </ul>
         </aside>
         <main>
           <Route path="/icon" component={IconExample} />
           <Route path="/button" component={ButtonExample} />
+          <Route path="/dialog" component={DialogExample}/>
         </main>
       </div>
     </div>
