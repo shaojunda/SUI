4879c79b815ddcb332299058659b15bda2e32d3d
shaojunda <shaojunda@gmail.com>
2019-05-12 21:24:23 +0800

    feat: add confirm and modal dialog

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,9 +1,14 @@
 import React, {useState} from 'react';
-import Dialog, {alert} from './dialog';
+import Dialog, {alert, confirm, modal} from './dialog';
 
 export default function () {
   const [visible, setVisible] = useState(false);
   const [visible2, setVisible2] = useState(false);
+  const openModal = () => {
+    const close = modal(<h1>你好
+      <button onClick={() => close()}>close</button>
+    </h1>);
+  };
   return (
     <div>
       <div>
@@ -33,6 +38,18 @@ export default function () {
       <div>
         <h1>Example 3</h1>
         <button onClick={() => {alert('1')}}>alert</button>
+        <button onClick={() => {
+          confirm('confirm', () => {
+            console.log("用户点击了 yes")
+          }, () => {
+            console.log("用户点击了 no")
+          })
+        }}>confirm
+        </button>
+      </div>
+      <div>
+        <h1>Example 4 </h1>
+        <button onClick={() => {openModal()}}>modal</button>
       </div>
     </div>
   )
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, ReactElement} from 'react';
+import React, {Fragment, ReactElement, ReactFragment, ReactNode} from 'react';
 import './dialog.scss'
 import {Icon} from "../index";
 import {scopedClassMaker} from "../classes";
@@ -32,11 +32,14 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
         </div>
         <header className={scopedClass('header')}>提示</header>
         <main className={scopedClass('main')}>{props.children}</main>
-        <footer className={scopedClass('footer')}>
-          {props.buttons && props.buttons.map((button, index) => {
-            React.cloneElement(button, {key: index})
-          })}
-        </footer>
+        {
+          props.buttons && props.buttons.length > 0 &&
+          <footer className={scopedClass('footer')}>
+            {props.buttons && props.buttons.map((button, index) =>
+              React.cloneElement(button, {key: index})
+            )}
+          </footer>
+        }
       </div>
     </Fragment> :
     null;
@@ -50,14 +53,58 @@ Dialog.defaultProps = {
 };
 
 const alert = (content: string) => {
-  const component = <Dialog onClose={() => {
+  const close = () => {
     ReactDOM.render(React.cloneElement(component, {visible: false}), div);
     ReactDOM.unmountComponentAtNode(div);
-  }} visible={true}>{content}</Dialog>;
+    div.remove();
+  };
+  const component = <Dialog onClose={close} visible={true}>{content}</Dialog>;
   const div = document.createElement("div");
   document.body.append(div);
   ReactDOM.render(component, div)
 };
 
-export {alert};
+const confirm = (content: string, yes?: () => void, no?: () => void) => {
+  const onYes = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    yes && yes();
+  };
+  const onNo = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    no && no();
+  };
+  const component = (
+    <Dialog onClose={() => {onNo()}}
+            visible={true}
+            buttons={[
+              <button onClick={onYes}>yes</button>,
+              <button onClick={onNo}>no</button>
+            ]}>
+      {content}
+    </Dialog>);
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  ReactDOM.render(component, div);
+};
+
+const modal = (content: ReactNode | ReactFragment) => {
+  const close = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+  };
+  const component = <Dialog onClose={() => {close()}} visible={true}>
+    {content}
+  </Dialog>;
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  ReactDOM.render(component, div);
+  return close;
+};
+
+export {alert, confirm, modal};
 export default Dialog;
